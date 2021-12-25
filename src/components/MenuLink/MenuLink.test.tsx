4d1f1b396e597f4435320e8b70dc3c0f6c0947cc
Alex Maques Alves <atanaelleonardo@gmail.com>
2021-12-25 16:15:36 -0300

    Ajustando componente HtmlContent

--- src/components/MenuLink/MenuLink.test.tsx
+++ src/components/MenuLink/MenuLink.test.tsx
@@ -40,27 +40,19 @@ describe('<MenuLink />', () => {
     expect(container.firstChild).toMatchInlineSnapshot(`
       .c0 {
         display: block;
+        color: #FFFFFF;
         -webkit-text-decoration: none;
         text-decoration: none;
-        font-size: 1.6rem;
-        padding: 1.6rem;
-        position: relative;
-      }
-
-      .c0::after {
-        content: '';
-        position: absolute;
-        bottom: 0.76rem;
-        left: 50%;
-        width: 0;
-        height: 0.2rem;
+        margin-bottom: 1.6rem;
+        font-size: 1.8rem;
+        border-right: 0.5rem solid #000000;
         -webkit-transition: all 300ms ease-in-out;
         transition: all 300ms ease-in-out;
       }
 
-      .c0:hover::after {
-        left: 25%;
-        width: 50%;
+      .c0:hover: {
+        border-right: 0.5rem solid #dc143c;
+        color: #dc143c;
       }
 
       <a
