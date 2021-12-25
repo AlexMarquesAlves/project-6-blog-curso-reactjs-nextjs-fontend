c3db6b3799d182e31a4ab74ec51d09b3624149b5
Alex Maques Alves <atanaelleonardo@gmail.com>
2021-12-25 16:45:57 -0300

    CSS do HtmlContent com Styled-Components

--- src/components/Footer/Footer.test.tsx
+++ src/components/Footer/Footer.test.tsx
@@ -18,8 +18,100 @@ describe('<Footer />', () => {
         text-decoration: none;
       }
 
-      .c1 {
+      500;700&display=swap'); .c1 {
         font-size: 2.4rem;
+      }
+
+      .c1 p {
+        margin: 4.0rem 0;
+      }
+
+      .c1 a,
+      .c1 a:visited,
+      .c1 a:link {
+        color: #dc143c;
+        -webkit-text-decoration: none;
+        text-decoration: none;
+        -webkit-transition: all 300ms ease-in-out;
+        transition: all 300ms ease-in-out;
+      }
+
+      .c1 a:hover {
+        -webkit-filter: brightness(50%);
+        filter: brightness(50%);
+      }
+
+      .c1 pre {
+        background: #000000;
+        padding: 2.4rem;
+        font-family: 'Fira Code';
+        color: #FFFFFF;
+        margin: 4.0rem 0;
+        width: 100%;
+        overflow-x: auto;
+        font-size: 1.6rem;
+      }
+
+      .c1 img {
+        max-width: 100%;
+      }
+
+      .c1 .image {
+        background: #DDDDDD;
+        line-height: 0;
+        margin: 4.0rem 0;
+      }
+
+      .c1 .image figcaption {
+        font-size: 1.6rem;
+        padding: 1.6rem;
+        text-align: center;
+        line-height: 1.3;
+      }
+
+      .c1 .image-style-side {
+        max-width: 50%;
+        float: right;
+        margin: 2.4rem;
+      }
+
+      .c1 hr {
+        border: none;
+        border-bottom: 0.1rem solid #DDDDDD;
+      }
+
+      .c1 ul,
+      .c1 ol {
+        margin: 4.0rem;
+      }
+
+      .c1 .table {
+        width: 100%;
+        overflow: hidden;
+        overflow-x: auto;
+      }
+
+      .c1 table {
+        width: 100%;
+        border-collapse: collapse;
+      }
+
+      .c1 table td,
+      .c1 table th {
+        padding: 1.6rem;
+        border: 0.1rem solid #DDDDDD;
+      }
+
+      @media (max-width:768px) {
+        .c1 {
+          font-size: 2rem;
+        }
+
+        .c1 .image-style-side {
+          max-width: 100%;
+          float: none;
+          margin: 0;
+        }
       }
 
       <div>
--- src/styles/global-styles.ts
+++ src/styles/global-styles.ts
@@ -1,6 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
+
   * {
     margin: 0;
     padding: 0;
@@ -25,7 +26,7 @@ export const GlobalStyles = createGlobalStyle`
     color: ${({ theme }) => theme.colors.primary};
   }
 
-  p {
+  /* p {
     margin: ${({ theme }) => theme.spacings.medium} 0;
   }
 
@@ -41,5 +42,5 @@ export const GlobalStyles = createGlobalStyle`
   .table {
     width: 100%;
     overflow-y: auto;
-  }
+  } */
 `;
