63e4287df3f9c7d933b5a47a78c58f11460c1532
Alex Maques Alves <atanaelleonardo@gmail.com>
2021-12-26 00:35:10 -0300

    Componente Header

---
+++ src/components/Header/mock.ts
@@ -0,0 +1,9 @@
+import { HeaderProps } from '.';
+
+export default {
+  blogName: 'Alex M. Alves',
+  blogDescription:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.',
+  logo: '/assets/images/Logo.jpg',
+  showText: true,
+} as HeaderProps;
