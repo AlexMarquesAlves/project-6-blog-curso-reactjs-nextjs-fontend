ed86c0c6ca8044c09d6f82160d03ae023cd8bca8
Alex Maques Alves <atanaelleonardo@gmail.com>
2021-12-28 01:26:04 -0300

    Componente PostTags

---
+++ src/components/PostTags/index.tsx
@@ -0,0 +1,28 @@
+import { PostTag } from '../../shared-typed/tag';
+import * as Styled from './styles';
+import Link from 'next/link';
+
+export type PostTagsProps = {
+  tags?: PostTag[];
+};
+
+export const PostTags = ({ tags = [] }: PostTagsProps) => {
+  if (tags.length === 0) {
+    return null;
+  }
+
+  return (
+    <Styled.Wrapper>
+      tags:{'  '}
+      {tags.map((tag) => {
+        return (
+          <span key={tag.displayName}>
+            <Link href={`/tag/${tag.slug}`}>
+              <a>{tag.displayName}</a>
+            </Link>
+          </span>
+        );
+      })}
+    </Styled.Wrapper>
+  );
+};
