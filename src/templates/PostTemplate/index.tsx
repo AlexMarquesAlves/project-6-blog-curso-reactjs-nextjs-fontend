0840b80a94bb24f13671ae7c97baf4ba4fcf7a26
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-01-03 19:14:30 -0300

    Componente Comments

--- src/templates/PostTemplate/index.tsx
+++ src/templates/PostTemplate/index.tsx
@@ -4,6 +4,7 @@ import { PostTags } from '../../components/PostTags';
 import { PostStrapi } from '../../shared-types/post-strapi';
 import { SettingsStrapi } from '../../shared-types/settings-strapi';
 import { BaseTemplate } from '../Base';
+import { Comments } from '../../components/Comments';
 
 export type PostTemplateProps = {
   settings: SettingsStrapi;
@@ -18,6 +19,13 @@ export const PostTemplate = ({ settings, post }: PostTemplateProps) => {
       <Styled.TagsContainer>
         <PostTags tags={post.tags} />
       </Styled.TagsContainer>
+
+      <Comments
+        title={post.title}
+        slug={post.slug}
+        id={post.id}
+        allowComments={post.allowComments}
+      />
     </BaseTemplate>
   );
 };
