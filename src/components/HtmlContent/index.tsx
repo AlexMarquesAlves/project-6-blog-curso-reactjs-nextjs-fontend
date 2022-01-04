fea9a1f0b6b11923c9d3bdd8e0e39f631b8f53fc
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-01-03 23:31:24 -0300

    jan_03_22 removeAds

--- src/components/HtmlContent/index.tsx
+++ src/components/HtmlContent/index.tsx
@@ -9,9 +9,18 @@ export type HtmlContentProps = {
 
 export const HtmlContent = ({ html }: HtmlContentProps) => {
   useEffect(() => {
+    let removeAds = null;
     if (typeof window !== undefined) {
       Prism.highlightAll();
+
+      removeAds = setTimeout(() => {
+        document
+          .querySelectorAll('iframe[src*=ads]')
+          .forEach((el) => el.remove());
+      }, 1000);
     }
+
+    return () => clearTimeout(removeAds);
   }, []);
 
   return (
